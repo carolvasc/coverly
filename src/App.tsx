bb1efd14b722e03d5f376791f950062063b465ac
Carolina Souza <carolina.vsouza@totvs.com.br>
2025-09-19 18:53:00 -0300

    feat: adicionando rotas ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import BookDetail from './components/BookDetail';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/book/:id" element={<BookDetail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
